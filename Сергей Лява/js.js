42d73c65ee64fa202d90418e6dedbc6587244cd7
itschoolgomother <itschoolgomother@gmail.com>
2021-02-17 20:10:50 +0200

    first commit

---
+++ Сергей Лява/js.js
@@ -0,0 +1,15 @@
+function dev(s){
+let	num = 5;
+for (let i = 0; i <= s ; i++ ){
+num *= num
+
+}
+return num;
+}
+
+let result = 0;
+let step = 3;
+let student = 10;
+for(let i = 0; i <= student; i+=step){
+	result+=i;
+}
